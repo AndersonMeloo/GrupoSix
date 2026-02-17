339a2d2b86e7322b941592f7ee58ec2f15fb03b7
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-02-17 16:08:11 -0300

    chore: component Garantia

---
+++ src/app/components/Garantia.tsx
@@ -0,0 +1,31 @@
+export function Garantia() {
+
+  return (
+
+    <>
+      <section className="py-10 flex justify-center px-4">
+        <div className="bg-[#f8fbff] rounded-xl shadow-md w-full max-w-sm p-6 sm:p-7 text-center items-center">
+
+          <h2 className="text-lg sm:text-xl font-bold text-gray-900">
+            🛡 Garantia de 30 Dias 100% de Volta
+          </h2>
+
+          <p className="text-gray-700 text-sm mt-4 leading-relaxed">
+            Se você não ficar completamente satisfeito nos próximos 30 dias,
+            devolveremos seu dinheiro integral, sem perguntas.
+          </p>
+
+          <div className="mt-6 bg-white rounded-lg shadow-sm p-4">
+            <p className="text-gray-600 text-sm">
+              Sua satisfação é nossa prioridade número 1
+            </p>
+
+            <p className="text-black font-semibold mt-2">
+              ✔ Garantia livre de risco
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
